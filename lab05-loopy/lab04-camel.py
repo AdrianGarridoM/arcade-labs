e33fb11e89a7f378af7273415a1c6263f51a7b5b
AdrianGarridoM <61007259+AdrianGarridoM@users.noreply.github.com>
2020-03-12 17:23:40 +0100

    Update lab04-camel.py

--- lab05-loopy/lab04-camel.py
+++ lab05-loopy/lab04-camel.py
@@ -34,14 +34,23 @@ def draw_section_2():
 
 
 def draw_section_3():
-    # Use the modulus operator and an if/else statement to select the color.
-    # Don't use multiple 'if' statements.
-    pass
+    for row in range(30):
+        for column in range(30):
+            x = column * 10 + 605
+            y = row * 10 + 5
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+            if row % 2 != 0:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
 
 
 def draw_section_4():
-    # Use the modulus operator and just one 'if' statement to select the color.
-    pass
+    for row in range(30):
+        for column in range(30):
+            x = column * 10 + 905
+            y = row * 10 + 5
+            arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.WHITE)
+            if row % 2 != 0 or column % 2 != 0:
+                arcade.draw_rectangle_filled(x, y, 5, 5, arcade.color.BLACK)
 
 
 def draw_section_5():
